96494ec59690ef58a73a38700b1652a191511ee3
Erin Millard <ezzatron@gmail.com>
2019-08-11 11:20:55 +1000

    Remove janky server push

--- src/server/create.js
+++ src/server/create.js
@@ -7,7 +7,6 @@ import {resolve} from 'path'
 
 import App from '../client/component/App.js'
 import createWebpackConfig from '../../webpack.config.js'
-import {close, fstat, open, readFile} from './fs.js'
 import {readOutputPath, readPublicPath, readStats} from './webpack.js'
 import {readTemplate} from './template.js'
 
@@ -20,19 +19,7 @@ export async function createServer () {
   const templatePath = resolve(__dirname, 'app.html')
   const appTemplate = await readTemplate(templatePath)
 
-  const rootPath = resolve(__dirname, '../..')
-  const certPath = resolve(rootPath, 'artifacts/cert')
-  const [httpsCert, httpsKey] = await Promise.all([
-    readFile(resolve(certPath, 'cert.pem')),
-    readFile(resolve(certPath, 'key.pem')),
-  ])
-
   const server = fastify({
-    http2: true,
-    https: {
-      cert: httpsCert,
-      key: httpsKey,
-    },
     logger: true,
   })
 
@@ -56,27 +43,7 @@ export async function createServer () {
       .concat(stylesheets.map(asset => [asset, 'style']))
       .concat(scripts.map(asset => [asset, 'script']))
 
-    // for (const asset of stylesheets) {
-    //   pushAssets.push([asset, 'style'])
-    //   // linkEntries.push(`<${pushAsset}>; rel=preload; as=style`)
-    // }
-
-    // for (const asset of scripts) {
-    //   pushAssets.push([asset, 'script'])
-    //   // linkEntries.push(`<${webpackPublicPath}${script}>; rel=preload; as=script`)
-    // }
-
     if (pushAssets.length > 0) {
-      const {res: {stream}} = reply
-
-      // await Promise.all(pushAssets.map(async ([asset, type]) => {
-      //   const filePath = resolve(webpackOutputPath, asset)
-      //   const publicPath = webpackPublicPath + asset
-      //   const mimeType = type === 'script' ? 'text/javascript' : 'text/css'
-
-      //   return push(stream, filePath, publicPath, mimeType)
-      // }))
-
       const linkHeaderValue = pushAssets
         .map(([asset, type]) => `<${webpackPublicPath}${asset}>; rel=preload; as=${type}`)
         .join(', ')
@@ -92,32 +59,4 @@ export async function createServer () {
   })
 
   return server
-}
-
-async function push (stream, filePath, publicPath, mimeType) {
-  const fd = await open(filePath)
-  let pushStream, stat
-
-  try {
-    stat = await fstat(fd)
-    pushStream = await createPushStream(stream, {':path': publicPath})
-  } catch (error) {
-    await close(fd)
-  }
-
-  pushStream.respondWithFD(fd, {
-    'content-length': stat.size,
-    'content-type': mimeType,
-    'last-modified': stat.mtime.toUTCString(),
-  })
-
-  pushStream.once('close', () => { close(fd).catch(() => {}) })
-}
-
-function createPushStream (stream, ...args) {
-  return new Promise((resolve, reject) => {
-    stream.pushStream(...args, (error, pushStream) => {
-      error ? reject(error) : resolve(pushStream)
-    })
-  })
 }
--- src/server/fs.js
+++ src/server/fs.js
@@ -1,13 +1,4 @@
 import {promisify} from 'util'
+import {readFile as readFileCps} from 'fs'
 
-import {
-  close as closeCps,
-  fstat as fstatCps,
-  open as openCps,
-  readFile as readFileCps,
-} from 'fs'
-
-export const close = promisify(closeCps)
-export const fstat = promisify(fstatCps)
-export const open = promisify(openCps)
 export const readFile = promisify(readFileCps)
