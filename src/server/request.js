c0dd125a3b53b8bcdc810dc7eb17386cad88b230
Erin Millard <ezzatron@gmail.com>
2019-08-23 13:17:00 +1000

    Improved error page

--- src/server/request.js
+++ src/server/request.js
@@ -7,11 +7,18 @@ export function createRequestHandler () {
     const {app, error} = handleRequest
 
     if (error) {
-      const errorHtml = ansi.toHtml(error.message)
+      const errorHtml = ansi.toHtml(trimError(error))
 
-      const style = '<style>body { font-size: 16px; background-color: #222; color: #EEE; }</style>'
-      const head = `<head><title>Error</title>${style}</head>`
-      const body = `<body><pre><code>${errorHtml}</code></pre></body>`
+      const style =
+        '<style>' +
+        'body { margin: 40px; background-color: #333; color: #EEE; }' +
+        'h1 { font-size: 50px; font-weight: 400; font-family: sans-serif; }' +
+        'pre { font-size: 20px; padding: 1em; overflow: auto; background-color: #222; }' +
+        '</style>'
+      const head = `<head><title>Server Error</title>${style}</head>`
+      const title = '<h1>Server Error</h1>'
+      const code = `<pre><code>${errorHtml}</code></pre>`
+      const body = `<body>${title}${code}</body>`
       const html = `<html>${head}${body}</html>`
 
       response.writeHead(500, 'Internal Server Error', {
@@ -27,4 +34,10 @@ export function createRequestHandler () {
   }
 
   return handleRequest
+}
+
+function trimError (error) {
+  const {message} = error
+
+  return message.replace(/.*Module build failed.*\n([\s\S]*?)\s+at [\s\S]*$/, '$1')
 }
