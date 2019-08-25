efd2af712f84e928d03a54eac548086cea9a008f
Erin Millard <ezzatron@gmail.com>
2019-08-25 18:47:45 +1000

    Use an express sub-app

--- src/server/main.js
+++ src/server/main.js
@@ -1,4 +1,12 @@
+import express from 'express'
+
 import {asyncMiddleware} from './express.js'
 import {createServerMiddleware} from './middleware.js'
 
-export default options => asyncMiddleware(createServerMiddleware(options))
+export default function createApp (options) {
+  const app = express()
+
+  app.use(asyncMiddleware(createServerMiddleware(options)))
+
+  return app
+}
