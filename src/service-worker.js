11e4d5f98a1aaac95e97c37b7c4f8b6254fb53e6
Erin Millard <ezzatron@gmail.com>
2019-09-14 15:07:45 +1000

    Use router5 in the service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,17 @@
 /* eslint-env worker */
 
-import pathToRegexp from 'path-to-regexp'
+import {cloneRouter} from 'router5'
 import {createHandlerForURL, precacheAndRoute} from 'workbox-precaching'
-import {NavigationRoute, registerRoute} from 'workbox-routing'
+import {registerRoute} from 'workbox-routing'
 
+import {createRouter, startRouter} from '~/src/routing.js'
 import routes from '~/src/routes.js'
 
-const navigationWhitelist = routes
-  .filter(({isClient = true}) => isClient)
-  .map(({path}) => pathToRegexp(path))
+const routesByName = routes.reduce((routes, route) => {
+  routes[route.name] = route
+
+  return routes
+}, {})
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
@@ -16,11 +19,43 @@ self.addEventListener('message', event => {
 
 precacheAndRoute(self.__WB_MANIFEST)
 
-const handler = createHandlerForURL(findAppShellUrl(self.__WB_MANIFEST))
-const navigationRoute = new NavigationRoute(handler, {
-  whitelist: navigationWhitelist,
-})
-registerRoute(navigationRoute)
+const baseRouter = createRouter(routes)
+const appShellHandler = createHandlerForURL(findAppShellUrl(self.__WB_MANIFEST))
+
+registerRoute(
+  function match (options) {
+    const {request, url: {pathname}} = options
+
+    if (request && request.mode !== 'navigate') return false
+    const match = baseRouter.matchPath(pathname)
+    if (!match) return false
+
+    const {isClient = true} = routesByName[match.name]
+
+    return isClient
+  },
+
+  async function handle (options) {
+    const {url: {pathname}} = options
+
+    const router = cloneRouter(baseRouter)
+    const routerState = await startRouter(router, pathname)
+
+    const {
+      name,
+      params,
+      meta: {
+        options: {
+          redirected: isRedirect,
+        } = {},
+      } = {},
+    } = routerState
+
+    if (isRedirect) return Response.redirect(router.buildPath(name, params), 302)
+
+    return appShellHandler(options)
+  },
+)
 
 function findAppShellUrl (manifest) {
   for (const {url} of manifest) {
