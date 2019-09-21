cd264200a0995b7db81fb75b8b216138694828a2
Erin Millard <ezzatron@gmail.com>
2019-09-21 22:04:11 +1000

    Implement routeTransition()

--- src/packula/router/transition-resolver.js
+++ src/packula/router/transition-resolver.js
@@ -3,6 +3,7 @@ export function createTransitionResolver (router) {
 
   return {
     routeNodes,
+    routeTransition,
   }
 
   function routeNodes (name) {
@@ -11,8 +12,33 @@ export function createTransitionResolver (router) {
     do {
       nodes.push(name)
       name = getRoute(name).parent
-    } while (typeof name !== 'undefined')
+    } while (name)
 
     return nodes.reverse()
+  }
+
+  function routeTransition (fromName, toName) {
+    if (!fromName) return {intersection: [], toActivate: routeNodes(toName), toDeactivate: []}
+
+    const fromNodes = routeNodes(fromName)
+    const toNodes = routeNodes(toName)
+    const intersection = findIntersection(fromNodes, toNodes)
+    const {length} = intersection
+
+    return {
+      intersection,
+      toActivate: toNodes.slice(length),
+      toDeactivate: fromNodes.slice(length).reverse(),
+    }
+  }
+
+  function findIntersection (fromNodes, toNodes) {
+    const limit = Math.min(fromNodes.length, toNodes.length)
+
+    for (let i = 0; i < limit; ++i) {
+      if (fromNodes[i] !== toNodes[i]) return toNodes.slice(0, i)
+    }
+
+    return toNodes.slice(0, limit)
   }
 }
--- test/unit/packula/router/transition-resolver.test.js
+++ test/unit/packula/router/transition-resolver.test.js
@@ -23,4 +23,130 @@ describe('Packula router transition resolver', () => {
       expect(routeNodes('routeC')).toStrictEqual([ROOT, 'routeA', 'routeB', 'routeC'])
     })
   })
+
+  describe('routeTransition', () => {
+    test('throws when an undefined from route is passed', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeB: {path: '/route-b'},
+      }))
+
+      expect(() => routeTransition('routeA', 'routeB')).toThrow('Undefined route "routeA"')
+    })
+
+    test('throws when an undefined to route is passed', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {path: '/route-a'},
+      }))
+
+      expect(() => routeTransition('routeA', 'routeB')).toThrow('Undefined route "routeB"')
+    })
+
+    test('returns correct route nodes when an empty value is passed as the from parameter', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+        routeC: {parent: 'routeB'},
+      }))
+
+      expect(routeTransition(null, 'routeA')).toStrictEqual({
+        intersection: [],
+        toActivate: [ROOT, 'routeA'],
+        toDeactivate: [],
+      })
+      expect(routeTransition(null, 'routeB')).toStrictEqual({
+        intersection: [],
+        toActivate: [ROOT, 'routeA', 'routeB'],
+        toDeactivate: [],
+      })
+      expect(routeTransition(null, 'routeC')).toStrictEqual({
+        intersection: [],
+        toActivate: [ROOT, 'routeA', 'routeB', 'routeC'],
+        toDeactivate: [],
+      })
+    })
+
+    test('returns correct route nodes for a transition to a child', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+      }))
+
+      expect(routeTransition('routeA', 'routeB')).toStrictEqual({
+        intersection: [ROOT, 'routeA'],
+        toActivate: ['routeB'],
+        toDeactivate: [],
+      })
+    })
+
+    test('returns correct route nodes for a transition from a child', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+      }))
+
+      expect(routeTransition('routeB', 'routeA')).toStrictEqual({
+        intersection: [ROOT, 'routeA'],
+        toActivate: [],
+        toDeactivate: ['routeB'],
+      })
+    })
+
+    test('returns correct route nodes for a transition to a grandchild', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+        routeC: {parent: 'routeB'},
+      }))
+
+      expect(routeTransition('routeA', 'routeC')).toStrictEqual({
+        intersection: [ROOT, 'routeA'],
+        toActivate: ['routeB', 'routeC'],
+        toDeactivate: [],
+      })
+    })
+
+    test('returns correct route nodes for a transition from a grandchild', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+        routeC: {parent: 'routeB'},
+      }))
+
+      expect(routeTransition('routeC', 'routeA')).toStrictEqual({
+        intersection: [ROOT, 'routeA'],
+        toActivate: [],
+        toDeactivate: ['routeC', 'routeB'],
+      })
+    })
+
+    test('returns correct route nodes for a transition between siblings', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+        routeC: {parent: 'routeA'},
+      }))
+
+      expect(routeTransition('routeB', 'routeC')).toStrictEqual({
+        intersection: [ROOT, 'routeA'],
+        toActivate: ['routeC'],
+        toDeactivate: ['routeB'],
+      })
+    })
+
+    test('returns correct route nodes for a transition between cousins', () => {
+      const {routeTransition} = createTransitionResolver(createRouter({
+        routeA: {},
+        routeB: {parent: 'routeA'},
+        routeC: {parent: 'routeB'},
+        routeD: {parent: 'routeA'},
+        routeE: {parent: 'routeD'},
+      }))
+
+      expect(routeTransition('routeC', 'routeE')).toStrictEqual({
+        intersection: [ROOT, 'routeA'],
+        toActivate: ['routeD', 'routeE'],
+        toDeactivate: ['routeC', 'routeB'],
+      })
+    })
+  })
 })
